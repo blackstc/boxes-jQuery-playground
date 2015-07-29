14e8cdccdad9c5e34b1917414e1dd051c781531d
Blackstock <charlieblackstock@gmail.com>
2015-07-29 15:24:06 -0600

    added solutions for exercise 2

--- boxes.js
+++ boxes.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+$(document).ready(function() {
+  console.log("hello world");
+
+//Solutions for exercise 2
+  // $("#secretBox").css("background-color", "white");
+  // $("#secretBox").append("<h1>secret box!</h1>");
+  // $("#row1 div").addClass("boxType3");
+  // $("#row4").find("div").eq(3).hide();
+  // $(".boxType1").css("background-color", "white");
+  // $("#row2 :lt(2)").removeClass("box boxType2 boxType3");
+  // $('div').not(".row").not("#container").not("#secretBox").css("width", "2px");
+
+//Solutions for exercise 3
+
+});
